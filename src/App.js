966e2da9eea15baad38a42eec09f99072954bf8a
thuongx1bg <thuongx1bg@gmail.com>
2022-09-25 13:32:07 +0700

    a

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
     <div >
       <div className='container'>
       <Routes>
-        <Route path='/tictactoe_3' element={<Game height={3} numberToWin={3}></Game>}></Route>
-        <Route path='/tictactoe_5' element={<Game height={5} numberToWin={4}></Game>}></Route>
-        <Route path='/tictactoe_7' element={<Game height={7} numberToWin={5}></Game>}></Route>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route  path='/tictactoe_3' element={<Game height={3} numberToWin={3}></Game>}></Route>
+        <Route  path='/tictactoe_5' element={<Game height={5} numberToWin={4}></Game>}></Route>
+        <Route  path='/tictactoe_7' element={<Game height={7} numberToWin={5}></Game>}></Route>
+        <Route  path='/' element={<Home></Home>}></Route>
       </Routes>
       </div>  
     </div>
--- src/components/tictactoe/Home.js
+++ src/components/tictactoe/Home.js
@@ -6,9 +6,9 @@ const Home = () => {
         <div className='home-container'>
             <div className="topnav">
                 <Link className="active" to="/">Home</Link>
-                <a href="https://thuongx1bg.github.io/react_app_tictactoe/#/tictactoe_3">Chế độ 3x3</a>
-                <a href="/tictactoe_5">Chế độ 5x5</a>
-                <a href="/tictactoe_7">Chế độ 7x7</a>
+                <Link to="/tictactoe_3">Chế độ 3x3</Link>
+                <Link to="/tictactoe_5">Chế độ 5x5</Link>
+                <Link to="/tictactoe_7">Chế độ 7x7</Link>
             </div>
         </div>
     );
